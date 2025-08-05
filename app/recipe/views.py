ec4d8b88306ca1536c8e21223e2fa5ece7a7500a
phalsus <odafeomajuwa@gmail.com>
2025-08-05 14:21:29 +0100

    done with recipe and recipe details api

---
+++ app/recipe/views.py
@@ -0,0 +1,33 @@
+"""
+views for the recipe API.
+"""
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
+from core.models import Recipe
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """view for managing recipe APIs"""
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return appropriate serializer class"""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe"""
+        serializer.save(user=self.request.user)
+
--- app/user/urls.py
+++ app/user/urls.py
@@ -1,5 +1,5 @@
 """
-url mapping for the useer api
+url mapping for the user api
 """
 from django.urls import path
 
@@ -9,5 +9,7 @@ from user import views
 app_name = "user"
 
 urlpatterns = [
-    path("create/", views.CreateUserView.as_view(), name="create")
+    path("create/", views.CreateUserView.as_view(), name="create"),
+    path("token/", views.CreateTokenView.as_view(), name="token"),
+    path("me/", views.ManageUserView.as_view(), name="me"),
 ]
